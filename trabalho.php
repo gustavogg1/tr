b9b34a6ef8405857e9a566be12052ddd71ff96fe
gustavogg1 <110700338+gustavogg1@users.noreply.github.com>
2022-09-20 19:30:12 -0300

    Create trabalho.php

---
+++ trabalho.php
@@ -0,0 +1,8 @@
+# <?php
+    $endereco = 'localhost';
+    $usuario  = 'root';
+    $senha    = '';
+    $base     = 'aula';
+
+$conexao = mysqli_connect("localhost", "root", "root", "unicv_web");
+?>
