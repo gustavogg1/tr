b239c389674bc727d92d852b4e76a3868ba26c6e
gustavogg1 <110700338+gustavogg1@users.noreply.github.com>
2022-09-22 21:58:18 -0300

    Add files via upload

---
+++ select.php
@@ -0,0 +1,28 @@
+<table border="1">
+    <tr>
+        <td>id</td>
+        <td>descricao</td>
+        <td>marca</td>
+        <td>estoque</td>
+        <td>preco</td>
+    </tr>
+<?php
+    require('trabalho.php');
+
+    $retorno = mysqli_query($conexao, "SELECT * FROM produtos");
+
+    while($produtos = mysqli_fetch_assoc($retorno))
+    {
+        echo '<tr>';
+        echo '<td>'.$produtos['id'].'</td>';
+        echo '<td>'.$produtos['descricao'].'</td>';
+        echo '<td>'.$produtos['marca'].'</td>';
+        echo '<td>'.$produtos['estoque'].'</td>';
+        echo '<td>'.$produtos['estoque'].'</td>';
+        echo '<td><a href="delete.php?id='.$produtos['id'].'">Excluir</a></td>';
+        echo '</tr>';
+    }
+
+    mysqli_close($conexao);
+?>
+</table>
